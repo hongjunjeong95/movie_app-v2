9a088fe55c0f894c7a8652a8e65c2f62e74105b7
zpskek <wjdghdwns0@gmail.com>
2021-02-25 15:14:24 +0900

    Create Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter as Router, Route } from "react-router-dom";
+
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/" exact component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail}></Route>
+    </Router>
   );
 }
 
