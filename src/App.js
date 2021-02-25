1e5216139f61f5ffa3df26cd824f8595acb79ac8
zpskek <wjdghdwns0@gmail.com>
2021-02-25 15:24:01 +0900

    Create header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { HashRouter as Router, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Detail from "./routes/Detail";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
+      <Header></Header>
       <Route path="/" exact component={Home} />
       <Route path="/about" component={About} />
       <Route path="/movie/:id" component={Detail}></Route>
